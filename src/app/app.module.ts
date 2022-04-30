f7fbbe429f8b29d8228b8280229bdc93936285cd
Matteo Musti <94116882+MatteoMusti@users.noreply.github.com>
2022-04-30 16:49:10 +0200

    incomplete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { DumbComponent } from './dumb/dumb.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DumbComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
