1109e6dac3ca934651d9a8594ea290624ad2eaa8
Matteo Musti <94116882+MatteoMusti@users.noreply.github.com>
2022-05-03 10:17:11 +0200

    to finish

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -14,6 +14,7 @@ export class ShopComponent implements OnInit {
   selectedBeer!: Beer;
   selectedBeers: Beer[] = [];
 
+
   constructor(private beerService: BeerService) { }
 
   ngOnInit(): void {
@@ -22,17 +23,32 @@ export class ShopComponent implements OnInit {
     )
   }
 
-  getBeer() {
-    const selectedValue = (document.getElementById('beersList') as HTMLSelectElement).value;
-    const beerId = this.beers.find(beer => beer.name === selectedValue)?.id;
+  checkType(): void {
+    const type = (document.getElementById('typeList') as HTMLSelectElement).value;
+    const name = (document.getElementById('beersList') as HTMLSelectElement).value;
+    const beerId = this.beers.find(beer => beer.name === name)?.id;
 
-    this.beerService.getBeerById(beerId!).subscribe(data => this.selectedBeer = data);
+    this.beerService.getBeerById(beerId!).subscribe((data) => {
+      if (data.type === type){
+        this.selectedBeer = data
+      }
+    })
+  }
 
-    if (this.selectedBeers.includes(this.selectedBeer)) {
+  getBeer(): void {
+    const name = (document.getElementById('beersList') as HTMLSelectElement).value;
+    const type = (document.getElementById('typeList') as HTMLSelectElement).value;
+    const beerId = this.beers.find(beer => beer.name === name)?.id;
+
+    this.beerService.getBeerById(beerId!).subscribe((data) => {
+      if (this.selectedBeers.includes(data)) {
+          return this.selectedBeers
+      }
+      if (data.type === type) {     
+        this.selectedBeers.push(data);
+      }
       return this.selectedBeers
-    } this.selectedBeers.push(this.selectedBeer);
-
-    return this.selectedBeers
+    })
   }
 
 }
