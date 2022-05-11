fe5a47afc0b8cd000ed7bf59f106454b30183c36
Matteo Musti <94116882+MatteoMusti@users.noreply.github.com>
2022-05-11 16:14:26 +0200

    incomplete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { JokeComponent } from './joke/joke.component';
+import { LikedComponent } from './liked/liked.component';
+import { DislikedComponent } from './disliked/disliked.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JokeComponent,
+    LikedComponent,
+    DislikedComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
