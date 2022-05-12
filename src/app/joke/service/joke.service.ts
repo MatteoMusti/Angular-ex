d8d3b43dbdadbbc23bf947ceaa1546cfd7e1b907
Matteo Musti <94116882+MatteoMusti@users.noreply.github.com>
2022-05-12 17:50:27 +0200

    add: service

---
+++ src/app/joke/service/joke.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, repeat, retry, tap, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JokeService {
+
+  constructor(private http: HttpClient) { }
+
+  joke = {}
+  liked = [];
+  disliked = [];
+
+  getJoke() {
+    return this.http.get('https://v2.jokeapi.dev/joke/Any').pipe(
+      repeat({ delay: 5000 }),
+      retry(3),
+      catchError(this.handleError)
+    )
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent){
+      console.error('An error occurred:', error.error.message)
+    } else {
+      console.error(
+        `Backend returned code ${error.status} ` +
+        `body was: ${error.error}`);
+    }
+    return throwError(() => new Error(
+      'Something bad happened; please try again later')
+    )
+  }   
+}
